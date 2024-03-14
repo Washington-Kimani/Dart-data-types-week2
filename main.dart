d4e30f56037bc715a62aef7bdfb92a89a1828343
Washington Kimani <kimaniwashington39@gmail.com>
2024-03-14 07:12:28 +0300

    First Commit

---
+++ main.dart
@@ -0,0 +1,29 @@
+main (){
+  //String data type for my name
+  String name = "Washington Kimani";
+
+  print("My name is $name");
+  //Int data type for age
+  Int age = 22;
+
+  print("I am $age years old");
+
+  //List data type for my hobbies
+  List hobbies = ["coding", "singing", "playing guitar", "star gazing"];
+
+
+  print("My hobbies are as follows");
+  hobbies.forEach((hobby){
+    print(hobby);
+  });
+  //Map data type for friends ages
+  Map<String, int> ages = {'John': 21, 'Bryan': 26, 'Simon': 33,};
+  print("My friends' ages are: $ages");
+  //Boolean data type for me being a male
+  Bool male = true;
+  if(male){
+    print("$name is a male");
+  }else{
+    print("Gender is unknown");
+  }
+}
